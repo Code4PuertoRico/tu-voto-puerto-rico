c5a8ee027f975bb5ad7f5a85c68802cd2575bc20
Emmanuel Luciano <emma.luciano11@gmail.com>
2020-08-29 11:06:51 -0400

    Adds initial work for site

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,17 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+import PropTypes from "prop-types"
+
+const Header = () => (
+  <header className="shadow-lg border border-gray-700 border-t-0 border-b-4 border-r-0 border-l-0 px-4">
+    <div className="flex justify-between mx-auto max-w-4xl py-2">
+      <h1 className="text-2xl font-bold">Tu Voto</h1>
+      <a
+        href="mailto:paravotar@code4puertorico.org"
+        className="bg-gray-700 py-2 px-4 text-white"
+      >
+        Contáctanos
+      </a>
     </div>
   </header>
 )
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,10 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
+
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import Code4PR from "../images/code-4-pr.svg"
 import Header from "./header"
-import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -26,20 +20,28 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <main className="pt-8 max-w-4xl mx-auto">
+        <div className="w-11/12 mx-auto">{children}</div>
+      </main>
+      <footer className="bg-gray-700 py-3 px-4 mt-12 text-center">
+        <p className="text-white text-sm font-light">
+          El código de este proyecto es totalmente público y puedes verlo en{" "}
+          <a
+            className="font-bold hover:underline"
+            href="https://github.com/Code4PuertoRico/tu-voto-puerto-rico"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </p>
+        <img
+          className="w-8 mx-auto mt-3"
+          src={Code4PR}
+          alt="Logo de Code for Puerto Rico"
+        />
+      </footer>
     </>
   )
 }
